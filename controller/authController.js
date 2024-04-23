f43a18ea95529e1b54ef2bac71354100222359db
UdaysingharyaU <uday.singh@quantmhill.com>
2024-04-23 22:26:04 +0530

    infinite changes

--- controller/authController.js
+++ controller/authController.js
@@ -11,6 +11,9 @@ import ErrorHandler from '../utils/errorHandler.js';
 export const signUp = async (req, res) => {
     try {
         console.log(req.body)
+        const userEnteredPassword = req.body.newPassword;
+        console.log("entered Password",userEnteredPassword);
+        
         const result = await _signUp(req.body)
         res.status(200).json(result);
     } catch (error) {
